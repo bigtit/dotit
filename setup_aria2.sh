c7736ac73e22bc825f843ee3178ffc817f228ee5
bigtit <phkm@outlook.jp>
2018-03-22 16:50:59 -0400

    Add files via upload

---
+++ setup_aria2.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkdir -p ~/.config/aria2
+touch ~/.config/aria2/aria2.conf
+echo "dir=/home/$(whoami)/Downloads" > ~/.config/aria2/aria2.conf
+echo "rpc-secret=secret" >> ~/.config/aria2/aria2.conf
+touch ~/.config/aria2/session.lock
+curl https://raw.githubusercontent.com/gutenye/systemd-units/master/aria2/aria2.service >> aria2.service
+sudo cp aria2.service /etc/systemd/user/
+
+systemctl --user enable aria2
+systemctl --user start aria2
+
