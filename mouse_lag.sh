bff2558c9e21ac23e2eae9ea99ce3bcac8038905
bigtit <phkm@outlook.jp>
2018-04-23 22:54:11 -0400

    Delete mouse_lag.sh

--- mouse_lag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sudo -i
-echo N> /sys/module/drm_kms_helper/parameters/poll
-#echo "drm_kms_helper" >> /etc/modprobe.d/mouse-lag.conf
-echo "options drm_kms_helper poll=N">/etc/modprobe.d/mouse-lag.conf
