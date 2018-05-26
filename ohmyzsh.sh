72c901e9e9b6190a38804b68769377d8e5d45a3b
kfang2 <kfang2@binghamton.edu>
2018-05-26 13:05:59 -0400

    ohmyzsh config

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ "$#" -ne 1 ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
