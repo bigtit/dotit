b1c011b4674867e6d2ecc07a947323dbbdb40600
bigtit <phkm@outlook.jp>
2019-03-28 05:18:14 +0800

    Update write2ntfs.sh

--- write2ntfs.sh
+++ write2ntfs.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 # only for mac darwin
 os=$(uname)
-if [[ ${os} != "Darwin" ]]; then
-  echo "os type is not Darwin, exit"
-  exit -1
-fi
+[[ ${os} != "Darwin" ]] && echo "os type is not Darwin, exit" && exit -1
+
+[[ -f "/sbin/mount_ntfs" ]] && echo 'no mount_ntfs found' && exit 1
+
 type=$(file -b --mime-type /sbin/mount_ntfs|sed 's|/.*||')
-if [[ ${type} = "text" ]]; then
-  echo "/sbin/mount_ntfs is already replaced, exit"
-  exit -1
-fi
-mv /sbin/mount_ntfs /sbin/mount_ntfs_
+[[ ${type} = "text" ]] && echo "/sbin/mount_ntfs is already replaced, exit" && exit 1
+
+mv /sbin/mount_ntfs /sbin/mount_ntfs_bin
 cat <<EOT > /sbin/mount_ntfs
 #!/bin/sh
-/sbin/mount_ntfs_ -o rw,nobrowse "\$@"
+/sbin/mount_ntfs_bin -o rw,nobrowse "\$@"
 EOT
 chmod a+x /sbin/mount_ntfs
 echo "mission complete, please reboot manually"
