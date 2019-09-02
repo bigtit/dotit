bc1f6963be433362bd3e0db5f956d4e85580f896
bigtit <similars2008@zju.edu.cn>
2019-09-02 13:11:09 -0400

    Update aria2.sh

--- aria2.sh
+++ aria2.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
 mkdir -p ~/.config/aria2
 touch ~/.config/aria2/aria2.conf
-echo "dir=/home/$(whoami)/Downloads" > ~/.config/aria2/aria2.conf
-echo "rpc-secret=secret" >> ~/.config/aria2/aria2.conf
 touch ~/.config/aria2/session.lock
-#curl https://raw.githubusercontent.com/gutenye/systemd-units/master/aria2/aria2.service > aria2.service
-sudo cp ./aria2.service /etc/systemd/user/
+
+cat ./aria2.conf <<EOL
+dir=/home/$(whoami)/Downloads/
+rpc-secret=secret
+max-overall-download-limit=7M
+EOL
+mv ./aria2.conf ~/.config/aria2/
+
+cat ./aria2.service <<EOL
+[Unit]
+Description=Aria2 Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/aria2c --enable-rpc --rpc-listen-all --rpc-allow-origin-all --save-session %h/.config/aria2/session.lock --input-file %h/.config/aria2/session.lock --conf-path=%h/.config/aria2/aria2.conf
+
+[Install]
+WantedBy=default.target
+EOL
+sudo mv ./aria2.service /etc/systemd/user/
 
 systemctl --user enable aria2
 systemctl --user start aria2
-
