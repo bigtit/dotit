535773f0f3537b9a9846e5041ce28e69401f4da3
bigtit <phkm@outlook.jp>
2020-02-19 06:11:23 +0800

    user.js titlebar

--- user.js
+++ user.js
@@ -4,3 +4,4 @@ user_pref("services.sync.prefs.sync.browser.uiCustomization.state", true);
 user_pref("places.history.expiration.max_pages", 400);
 user_pref("services.sync.prefs.sync.places.history.expiration.max_pages", true);
 user_pref("services.sync.prefs.dangerously_allow_arbitrary", true);
+user_pref("browser.tabs.drawInTitlebar", true);
