cc1b0e7f05826dcc3e98453188c364fbe8e8a5f0
bigtit <phkm@outlook.jp>
2020-09-08 17:39:33 -0400

    Delete user.js

--- user.js
+++
@@ -1,9 +0,0 @@
-
-user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[\"_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"switchyomega_feliscatus_addons_mozilla_org-browser-action\",\"_e2488817-3d73-4013-850d-b66c5e42d505_-browser-action\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"home-button\",\"urlbar-container\",\"downloads-button\",\"sidebar-button\",\"fxa-toolbar-menu-button\",\"ublock0_raymondhill_net-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"new-tab-button\",\"alltabs-button\"],\"PersonalToolbar\":[\"personal-bookmarks\"]},\"seen\":[\"developer-button\",\"_7a7a4a92-a2a0-41d1-9fd7-1e92480d612d_-browser-action\",\"_e2488817-3d73-4013-850d-b66c5e42d505_-browser-action\",\"_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action\",\"ublock0_raymondhill_net-browser-action\",\"switchyomega_feliscatus_addons_mozilla_org-browser-action\"],\"dirtyAreaCache\":[\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\",\"PersonalToolbar\",\"widget-overflow-fixed-list\"],\"currentVersion\":16,\"newElementCount\":4}");
-user_pref("services.sync.prefs.sync.browser.uiCustomization.state", true);
-user_pref("places.history.expiration.max_pages", 400);
-user_pref("services.sync.prefs.sync.places.history.expiration.max_pages", true);
-user_pref("services.sync.prefs.dangerously_allow_arbitrary", true);
-user_pref("browser.tabs.drawInTitlebar", true);
-user_pref("general.smoothScroll", false);
-user_pref("browser.urlbar.autoFill", false);
